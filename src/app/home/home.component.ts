da3da58f77f594d42cd4aa8cb5f0d963003dfebc
Vignesh Selvaraj <s.vignesh07@gmail.com>
2019-10-22 23:48:50 +0530

    Add label, circles and a blue gradient chart area

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs/operators';
+import * as d3Fetch from 'd3-fetch';
+import * as d3TimeFormat from 'd3-time-format';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,32 @@ import { finalize } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   isLoading = false;
+  data: any;
+  sales_data: any;
+  new_sales_data: any;
+  payments_data: any;
+  refunds_data: any;
 
   constructor() {}
 
   ngOnInit() {
     this.isLoading = true;
+
+    var parseDate = d3TimeFormat.timeParse("%Y-%m-%d");
+
+    d3Fetch.json("../assets/data/line-chart.json").then((response) => {
+      this.data = response.map(( datum ) => {
+        datum.date = parseDate(datum.date);
+        return datum;
+      });
+
+      this.sales_data = this.data;
+      this.new_sales_data = this.data;
+      this.payments_data = this.data;
+      this.refunds_data = this.data;
+
+      this.isLoading = false;
+    });
+
   }
 }
--- src/app/shared/line-chart/line-chart.component.ts
+++ src/app/shared/line-chart/line-chart.component.ts
@@ -1,95 +1,194 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 import * as d3 from 'd3-selection';
 import * as d3Scale from 'd3-scale';
 import * as d3Shape from 'd3-shape';
 import * as d3Array from 'd3-array';
+import * as d3Interpolate from 'd3-interpolate';
+import * as d3Transition from 'd3-transition';
 import * as d3Axis from 'd3-axis';
-import * as d3Fetch from 'd3-fetch';
-import * as d3TimeFormat from 'd3-time-format';
+import * as d3Ease from 'd3-ease';
 
 @Component({
-    selector: 'app-line-chart',
-    encapsulation: ViewEncapsulation.None,
-    templateUrl: './line-chart.component.html',
-    styleUrls: ['./line-chart.component.scss']
+  selector: 'app-line-chart',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss']
 })
 export class LineChartComponent implements OnInit {
 
-    title = 'Line Chart';
+  @Input() elementId: String;
+  @Input() chartTitle: any;
+  @Input() chartData: any;
+  @ViewChild('container', { static: true }) container: ElementRef;
 
-    private margin = {top: 20, right: 20, bottom: 30, left: 50};
-    private width: number;
-    private height: number;
-    private x: any;
-    private y: any;
-    private svg: any;
-    private line: d3Shape.Line<[number, number]>;
-    data: any;
+  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+  private line: d3Shape.Line<[number, number]>;
+  private area;
 
-    constructor() {
-        this.width = 700 - this.margin.left - this.margin.right;
-        this.height = 400 - this.margin.top - this.margin.bottom;
+  DURATION = 1500;
+  DELAY = 500;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (!!this.chartData) {
+      setTimeout(() => {
+        this.initSvg();
+        this.initAxis();
+        this.drawAxis();
+        this.drawLine();
+        this.drawArea();
+        this.drawCircles();
+      }, 100);
     }
+  }
 
-    ngOnInit() {
-        var parseDate = d3TimeFormat.timeParse("%Y-%m-%d");
+  private initSvg() {
+    this.svg = d3.select(`svg#${this.elementId}`)
+      .append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  }
 
-        d3Fetch.json("../assets/data/line-chart.json").then((response) => {
-          this.data = response.map(( datum ) => {
-            datum.date = parseDate(datum.date);
-            return datum;
-          });
+  private initAxis() {
+    this.width = this.container.nativeElement.offsetWidth - this.margin.left - this.margin.right;
+    this.height = this.container.nativeElement.offsetHeight - this.margin.top - this.margin.bottom;
+    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain(d3Array.extent(this.chartData, (d) => d.date));
+    this.y.domain(d3Array.extent(this.chartData, (d) => d.value));
+  }
 
-          this.initSvg();
-          this.initAxis();
-          this.drawAxis();
-          this.drawLine();
+  private drawAxis() {
+
+    this.svg.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(d3Axis.axisBottom(this.x));
+
+    this.svg.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3Axis.axisLeft(this.y))
+      .append('text')
+      .attr('class', 'axis-title')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.75em')
+      .style('text-anchor', 'end')
+      .text('Price ($)');
+  }
+
+
+  private drawArea() {
+    this.area = d3Shape.area()
+      .x((d) => { return this.x(d.date) })
+      .y0(this.height)
+      .y1((d) => { return this.y(d.value); });
+
+    this.svg.append('path')
+      .datum(this.chartData)
+      .attr('class', 'lineChart--area')
+      .attr('d', this.area);
+
+      // d3Transition.transition().select('.lineChart--area')
+      // .transition()
+      // .duration(this.DURATION)
+      // .delay(this.DURATION / 2)
+      // .attrTween('d', this.tween(this.chartData, this.line));
+  }
+
+  private drawLine() {
+    this.line = d3Shape.line()
+      .x((d: any) => this.x(d.date))
+      .y((d: any) => this.y(d.value));
+
+    this.svg.append('path')
+      .datum(this.chartData)
+      .attr('class', 'lineChart--areaLine')
+      .attr('d', this.line);
+
+  }
+
+  private drawCircles() {
+    this.svg.selectAll(".dot")
+      .data(this.chartData)
+      .enter().append("circle") // Uses the enter().append() method
+      .attr("class", "dot lineChart--circle") // Assign a class for styling
+      .attr("cx", (d, i) => {
+        return this.x(d.date);
+      })
+      .attr("cy", (d) => {
+        return this.y(d.value);
+      })
+      .attr("r", 5)
+      .on("mouseenter", (d, i, nodes) => {
+        d3.select(nodes[i])
+          .attr('class', 'lineChart--circle lineChart--circle__highlighted')
+          .attr('r', 7);
+        this.showCircleDetail(d);
+      })
+      .on("mouseout", (d, i, nodes) => {
+        d3.select(nodes[i])
+          .attr(
+            'class',
+            'lineChart--circle'
+          )
+          .attr('r', 6);
+        this.hideCircleDetails()
+      });
+  }
+
+  private tween(b, callback) {
+    return (a) => {
+      var i = d3Interpolate.interpolateArray(a, b);
+      return (t) => callback(i(t));
+    };
+  }
+
+
+  private hideCircleDetails() {
+    d3.selectAll('.lineChart--bubble').remove();
+  }
+
+  private showCircleDetail(data) {
+    var details = d3.select('svg').append('g')
+      .attr('class', 'lineChart--bubble')
+      .attr(
+        'transform', () => {
+          var result = 'translate(';
+          result += this.x(data.date);
+          result += ', ';
+          result += this.y(data.value);
+          result += ')';
+          return result;
         });
-    }
 
-    private initSvg() {
-        this.svg = d3.select('svg')
-            .append('g')
-            .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-    }
+    details.append('rect')
+      .attr("fill", "#222")
+      .attr('width', 75)
+      .attr('height', 60)
+      .attr('rx', 10)
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-    private initAxis() {
-        this.x = d3Scale.scaleTime().range([0, this.width]);
-        this.y = d3Scale.scaleLinear().range([this.height, 0]);
-        this.x.domain(d3Array.extent(this.data, (d) => d.date ));
-        this.y.domain(d3Array.extent(this.data, (d) => d.value ));
-    }
+    var text = details.append('text')
+      .attr('class', 'lineChart--bubble--text');
 
-    private drawAxis() {
+    text.append('tspan')
+      .attr('class', 'lineChart--bubble--label')
+      .attr('x', this.margin.left + 10)
+      .attr('y', this.margin.top + 20)
+      .text(data.label);
 
-        this.svg.append('g')
-            .attr('class', 'axis axis--x')
-            .attr('transform', 'translate(0,' + this.height + ')')
-            .call(d3Axis.axisBottom(this.x));
-
-        this.svg.append('g')
-            .attr('class', 'axis axis--y')
-            .call(d3Axis.axisLeft(this.y))
-            .append('text')
-            .attr('class', 'axis-title')
-            .attr('transform', 'rotate(-90)')
-            .attr('y', 6)
-            .attr('dy', '.75em')
-            .style('text-anchor', 'end')
-            .text('Price ($)');
-    }
-
-
-    private drawLine() {
-      this.line = d3Shape.line()
-          .x( (d: any) => this.x(d.date) )
-          .y( (d: any) => this.y(d.value) );
-
-      this.svg.append('path')
-          .datum(this.data)
-          .attr('class', 'line')
-          .attr('d', this.line);;
-    }
+    text.append('tspan')
+      .attr('class', 'lineChart--bubble--value')
+      .attr('x', this.margin.left + 10)
+      .attr('y', this.margin.top + 50)
+      .text(data.value);
+  }
 
 }
